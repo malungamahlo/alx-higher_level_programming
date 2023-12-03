64d349eb215b3df8dcbbb6c53c0ad1ebbc1523ed
Malungamahlo <malungamahlo@gmail.com>
2023-12-03 01:24:05 -0800

    Upload all mondatory

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
-from sys import argv
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    num_args = len(argv) - 1
+    """Print the number of and list of arguments"""
+    import sys
 
-    print("{} argument{}:".format(num_args, "s" if num_args != 1 else ""), end="")
-    
-    if num_args == 0:
-        print(".")
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print()
-        for i, arg in enumerate(argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
