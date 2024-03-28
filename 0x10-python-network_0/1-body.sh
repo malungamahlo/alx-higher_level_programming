1e0d6fcd0c67888c9b87c4ba331f5d1d79232697
Malungamahlo <malungamahlo@gmail.com>
2024-03-28 03:37:33 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL,
-# and displays the body of the response (if status code is 200)
-curl -sL -w "%{http_code}" -o /dev/null "$1" | 
-if [ $(tail -n1) -eq 200 ]; then
-    curl -sL "$1"
-fi
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -Ls "$1"
